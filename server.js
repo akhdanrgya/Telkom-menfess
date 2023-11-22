55fed998cfb650898e97e41949d7cc2f3bdd996f
akhdanrgya <akhdan.anargya@gmail.com>
2023-11-22 23:28:22 +0700

    2

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = 5500
 
+require('dotenv').config()
 app.use(express.json())
+
+app.get('/tweet', (req, res) => {
+    res.json('Tweet DATA HERE')
+})
 
 app.get('/', (req, res) => {
     res.send('<h1> Welcome To Telkom Menfess </h1>')

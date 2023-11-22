4f591fbe2893c620ee0adbdf29701f42fa79d092
akhdanrgya <akhdan.anargya@gmail.com>
2023-11-22 23:49:09 +0700

    update middleware

--- server.js
+++ server.js
@@ -1,29 +1,33 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const tweetBot = require('./tweet-bot')
+const { TweetBot } = require("./tweet-bot");
 
-const app = express()
-const port = 5500
+const app = express();
+const port = 5500;
 
-require('dotenv').config()
-app.use(express.json())
+require("dotenv").config();
+app.use(express.json());
 
-
+const tweetBot = new TweetBot({
+  consumer_key: "",
+  consumer_secret: "",
+  acces_token: process.env.TWEET_ACCES_TOKEN,
+  acces_token_secret: process.env.TWEET_ACCES_TOKEN_SECRET,
+});
 
 // route section
-app.get('/tweet', (req, res) => {
-    res.json('Tweet DATA HERE')
-})
+app.get("/adminTweet", async (req, res, next) => {
+  const admin = await TweetBot.getAdminUserInfo();
+});
 
-app.get('/', (req, res) => {
-    res.send('<h1> Welcome To Telkom Menfess </h1>')
-})
+app.get("/", (req, res) => {
+  res.send("<h1> Welcome To Telkom Menfess </h1>");
+});
 // end route section
-
 
 // listen
 app.listen(port, () => {
-    console.log(`Listening server on port http://localhost:${port}`)
-})
+  console.log(`Listening server on port http://localhost:${port}`);
+});
 // end listen
--- tweet-bot.js
+++ tweet-bot.js
@@ -1,17 +1,26 @@
-const twit = require('twit')
+const twit = require("twit");
 
 class TweetBot {
-    constructor(props) {
-        this.T = new Twit({
-            consumer_key: props.consumer_key,
-            consumer_secret: props.consumer_secret,
-            acces_token: props.acces_token,
-            acces_token_secret: props.acces_token_secret
+  constructor(props) {
+    this.T = new Twit({
+      consumer_key: props.consumer_key,
+      consumer_secret: props.consumer_secret,
+      acces_token: props.acces_token,
+      acces_token_secret: props.acces_token_secret,
+    });
+  }
+
+  getAdminUserInfo = () => {
+    return new Promise((resolve, reject) => {
+      this.T.get("account/verify_credentials", { skip_status: true })
+        .then((result) => {
+          resolve(result);
         })
-    }
-
-    getAdminUserInfo = () => {
-        this.T.get('account/verify_credentials', { skip_status: true })
-    }
-
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  };
 }
+
+module.exports = { TweetBot }
